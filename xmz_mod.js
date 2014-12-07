dbd441d83ce6bad72a377170fb528c7b314d09c9
Stefan Müller <s.mueller@it.kls-glt.de>
2014-12-07 13:13:00 +0100

    Iron Router installiert und includiert

--- xmz_mod.js
+++ xmz_mod.js
@@ -1,2 +1,14 @@
 
 Sensors     = new Mongo.Collection("sensors");
+
+Router.route('/', function () {
+  this.render('sensorView');
+});
+
+Router.route('/sensors', function() {
+  this.render('sensorList');
+});
+
+Router.route('/newSensor', function() {
+  this.render('sensorForm');
+});
